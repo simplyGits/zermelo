7d3d7fae2f7d77d981b1d3d46aacec167656b1c9
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-09-08 20:34:26 +0200

    fix tonicdevExample.js

--- tonicdevExample.js
+++ tonicdevExample.js
@@ -1,12 +1,12 @@
 // Replace every '<thing>' with your credentials:
 
-const { createSession, loginBySessionInfo } = require('zermelo');
+const { createSession, loginBySessionInfo } = require('zermelo.js');
 
 // create a session
 const sessionInfo = await createSession('<schoolid>', '<authcode>');
 
 // create an Zermelo instance using the created session
-const zermelo = await loginBySessionInfo(sessionInfo);
+const zermelo = await loginBySessionInfo('<schoolid>', sessionInfo);
 
 // get the info of the logged in user
 const userInfo = await zermelo.userInfo();
